2dec80970d24e50363b3124cf7397699df1f66b9
mhuss044D430 <mhuss044@gmail.com>
2014-06-26 20:00:57 -0400

    screenshots

--- curget.cpp
+++ curget.cpp
@@ -1,12 +1,30 @@
 // do some curses stuff 
+
 #include <ncurses.h>
 #include <math.h>
 #include <stdlib.h>
 
+typedef struct _plyr
+{
+	float px, py;
+	_plyr()
+	{
+		px = 0.0;
+		py = 0.0;
+	}
+}plyr;
+
 int main(void)
 {
-	int c, winX, winY, cx, cy, ang = 0, r = 0, inc = 1;
+	int c = 'g',
+	// Draw ring vars 
+	int winX, winY, cx, cy, ang = 0, r = 0, inc = 1;
+	char inp;
 	bool gu = true;
+	// Game
+	float obstac[9] = {0,0,0,0,0,0,0,0,0};	// Topleft, scale, pos from cntr
+	// Player
+	plyr npl;
 
 	initscr();		/* Start curses mode */
 	raw();			/* Line buffering disabled */
@@ -46,48 +64,79 @@ int main(void)
 
 		
 	//attron(A_BOLD);
-	while(1)
+	cbreak();	// no wait for 'EOF' or carriage return
+	timeout(1);	// nonblocking
+
+	clear();	// clear scr
+	while(inp !='q')
 	{
+		inp = getch();
 		gu = !gu;
 		cx = 0;
 		cy = 0;
 		r = 0;
 
+		// Go fast effect?
+		inp == 'w' ? inc = 1 : inc = rand()%6+3;
+		// Move to next ring pos, draw it
 		while(winX*2 > (winX+cx) && winY*2 > (winY+cy))
 		{
 			gu = !gu;
 			ang = 0;
 
+			// Draw Ring
 			while(ang <= 360)
 			{
+				// Get x,y of next ring coord
 				cx = r*cos(float(ang*(0.01745329251)));	
 				cy = r*sin(float(ang*(0.01745329251)));
 
-//				if(winX*2 > (winX+cx) && winY*2 > (winY+cy))
-//				{
 					if(gu)
 					{
 						move(winY+cy,winX+cx);	// moves the cursor to rowth row and colth column 
-						addch('O');
+				//		addch('O');
+						addch(ACS_BULLET);
 					}
 					else
 					{
 						move(winY+cy,winX+cx);	// moves the cursor to rowth row and colth column 
-						addch('X');
+//						addch('X');
+//						addch(ACS_BLOCK);
+						addch(ACS_DIAMOND); 
 					}
 					ang += 1;
-					refresh();		// out
-//				}
-					
 			}
-			r += rand()%5+1;
+			r += inc;	// Get next radius
 		}
-		inc = rand()%10+1;
+		// Done drawing bkg
+
+		// Move player
+		switch(inp)
+		{
+			case 'a':
+				npl.px -= 2;
+				break;
+			case 'd':
+				npl.px += 2;
+				break;
+			case 's':
+				break;
+		}
+
+		// Draw player
+		for(int y = 0; y < 5; y++)
+		{
+			move(winY+npl.py-y,winX+npl.px);
+			addch('*');
+			for(int x = 1; x < y; y++)
+			{
+				move(winY+npl.py-y,winX+npl.px);
+				addch('*');
+			}
+		}
+		refresh();		// out
 	}
 
-
-	getch();	/* Wait for user input */
 	endwin();	/* End curses mode */
-			
 	return 0;
 }
